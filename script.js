06d3b431058a8509d0b0407911b323b91b08e3b7
Samiksha Bharti <115936670+Sami70458@users.noreply.github.com>
2024-10-18 18:15:07 +0000

    project2

---
+++ script.js
@@ -0,0 +1,36 @@
+
+const form = document.querySelector('form')
+
+//this use case will give you empty value
+//const height = parseInt(document.querySelector('#height').value)
+
+form.addEventListener('submit',function(e){
+    e.preventDefault()
+
+    const height = parseInt(document.querySelector('#height').value)
+    const weight = parseInt(document.querySelector('#weight').value)
+    const results = document.querySelector('#results')
+    
+    if(height === '' || height <0 || isNaN(height))
+    {
+        results.innerHTML = `Please give a valid height ${height}`
+    }else if(weight === '' || height <0 || isNaN(weight))
+    {
+        results.innerHTML = `Please give a valid weight ${weight}`
+    } else {
+       const bmi = (weight / ((height * height) / 10000)).toFixed(2);
+       let category = ''
+
+       if (bmi < 18.6) {
+           category = 'Underweight'
+       } else if (bmi >= 18.6 && bmi <= 24.9) {
+           category = 'Normal Range'
+       } else {
+           category = 'Overweight'
+       }
+
+       results.innerHTML = `<span>BMI: ${bmi} (${category})</span>`
+        }
+    }
+
+)
